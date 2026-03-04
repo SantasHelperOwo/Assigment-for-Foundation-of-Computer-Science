f6f70d0f995713a745d9c802efd9a9bffc88c43c
SantasHelperOwo <atipshrestha12@gmail.com>
2026-03-04 10:55:45 +0545

    Completed Task 4.4: Basic SQL Operations. Added queries_4.4.sql with INSERT and SELECT statements. Included screenshots of query outputs for Student and Club tables.

---
+++ Task3_Normalization/queries_4.4-Basic SQL Operations.sql
@@ -0,0 +1,5 @@
+INSERT INTO Student (StudentID, StudentName, Email)
+VALUES (8, 'Kabir', 'kabir@email.com');
+
+INSERT INTO Club (ClubID, ClubName, ClubRoom, ClubMentor)
+VALUES (105, 'Photography Club', 'R404', 'Ms. Tara');
